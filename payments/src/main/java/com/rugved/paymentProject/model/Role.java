6042dde6667af1c79e41a2cd5d5f851eb542fc05
Rugved7 <rugvedagasti7@gmail.com>
2025-09-18 02:44:59 +0530

    Roles Entity added

--- payments/src/main/java/com/rugved/paymentProject/model/Role.java
+++ payments/src/main/java/com/rugved/paymentProject/model/Role.java
@@ -1,4 +1,22 @@
 package com.rugved.paymentProject.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, unique = true)
+    private ERole name;
 }
