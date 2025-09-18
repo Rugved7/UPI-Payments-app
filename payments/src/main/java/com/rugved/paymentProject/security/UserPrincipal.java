17f037cceab5a6b17f81f6545db031ef64fff198
Rugved7 <rugvedagasti7@gmail.com>
2025-09-19 00:44:56 +0530

    Custom User Details added

---
+++ payments/src/main/java/com/rugved/paymentProject/security/UserPrincipal.java
@@ -0,0 +1,58 @@
+package com.rugved.paymentProject.security;
+
+import com.rugved.paymentProject.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+public class UserPrincipal implements UserDetails {
+    private Long id;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public static UserPrincipal create(User user) {
+        List<GrantedAuthority> authorityList = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
+
+        return new UserPrincipal(
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                authorityList);
+    }
+
+    @Override
+    public String getUsername(){
+        return email;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
