6107dfc1ddb7361c4bcf85caf940810e5911fffc
Rugved7 <rugvedagasti7@gmail.com>
2025-09-19 01:09:58 +0530

    Login Request and Jwt Response DTO added

---
+++ payments/src/main/java/com/rugved/paymentProject/dto/JwtResponse.java
@@ -0,0 +1,16 @@
+package com.rugved.paymentProject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String email;
+    private List<String> roles;
+}
---
+++ payments/src/main/java/com/rugved/paymentProject/dto/LoginRequest.java
@@ -0,0 +1,18 @@
+package com.rugved.paymentProject.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "Email is Required")
+    private String email;
+
+    @NotBlank(message = "Password is Required")
+    private String password;
+}
