b444f099002cc97ad5e28963d6c47c2450fbf3a6
Rugved7 <rugvedagasti7@gmail.com>
2025-09-21 20:11:55 +0530

    Async config added for thread safety and allow multithreading in UPI Service

---
+++ payments/src/main/java/com/rugved/paymentProject/config/AsyncConfig.java
@@ -0,0 +1,27 @@
+package com.rugved.paymentProject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "transactionTaskExecutor")
+    public Executor transactionTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(25);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("TransactionThread-");
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(30);
+        executor.initialize();
+        return executor;
+    }
+}
